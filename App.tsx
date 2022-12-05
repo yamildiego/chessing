9f6d73c7d1f96b614f982238f7d30c5237bd11a1
yamildiego <yamildiego@gmail.com>
2022-12-06 01:04:21 +1100

    refactor

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
+import React, { Suspense } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
@@ -7,17 +10,38 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducers from "./src/Reducers";
 
-import Game from "./src/Components/Game";
+import PreHomeScreen from "./src/Screens/PreHomeScreen";
+import MainScreen from "./src/Screens/MainScreen";
 
 const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)));
+const Stack = createNativeStackNavigator();
+
+const LazyPlaceholder = ({ route }) => (
+  <View style={styles.scene}>
+    <Text>Loading {route.title}…</Text>
+  </View>
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Provider store={store}>
-        <Game />
-      </Provider>
-      <StatusBar style="auto" />
+      <Suspense fallback={<Text>Loating./\</Text>}>
+        <Provider store={store}>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen options={{ headerShown: false }} name="PreHomeScreen" component={PreHomeScreen} />
+              <Stack.Screen options={{ headerShown: false }} name="MainScreen" component={MainScreen} />
+              {/* <Stack.Screen options={{ header: (props) => <Header {...props} /> }} name="HomeScreen" component={HomeScreen} />
+              <Stack.Screen
+                name="Login"
+                options={{ headerStyle: { backgroundColor: primaryColor }, headerTintColor: "#fff" }}
+                component={LoginScreen}
+              /> */}
+            </Stack.Navigator>
+          </NavigationContainer>
+        </Provider>
+        <StatusBar style="auto" />
+      </Suspense>
     </View>
   );
 }
